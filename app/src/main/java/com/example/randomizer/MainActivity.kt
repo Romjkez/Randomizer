04e07859430128ee057dd4d8e1ff15f673b5164d
Romjkez <meshkov.ra@ya.ru>
2019-09-21 12:03:33 +0300

    Add validation

--- app/src/main/java/com/example/randomizer/MainActivity.kt
+++ app/src/main/java/com/example/randomizer/MainActivity.kt
@@ -1,17 +1,44 @@
 package com.example.randomizer
 
 import android.os.Bundle
+import android.text.Editable
 import android.text.TextUtils.isEmpty
+import android.text.TextWatcher
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val textEditWatcher = object : TextWatcher {
+        override fun afterTextChanged(p0: Editable?) {
+        }
+
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        }
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            randomizeButton.isEnabled = isInputsValid()
+            randomizeButton.isClickable = isInputsValid()
+            if (!isInputsValid()) {
+                randomizeButton.setBackgroundColor(resources.getColor(R.color.colorDisabled))
+            } else {
+                randomizeButton.setBackgroundColor(resources.getColor(R.color.colorPrimary))
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         randomizeButton.setOnClickListener { randomize() }
+        randomizeButton.isEnabled = isInputsValid()
+        randomizeButton.isClickable = isInputsValid()
+        if (!isInputsValid()) {
+            randomizeButton.setBackgroundColor(resources.getColor(R.color.colorDisabled))
+        } else randomizeButton.setBackgroundColor(resources.getColor(R.color.colorPrimary))
+
+        minEdit.addTextChangedListener(textEditWatcher)
+        maxEdit.addTextChangedListener(textEditWatcher)
     }
 
     private fun randomize() {
@@ -21,8 +48,11 @@ class MainActivity : AppCompatActivity() {
             val result = (min..max).random()
             outputText.text = result.toString()
             Toast.makeText(this, "Your random number: $result", Toast.LENGTH_SHORT).show()
-        } else {
-            Toast.makeText(this, "Enter min and max value", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun isInputsValid(): Boolean {
+        return !isEmpty(minEdit.text) && !isEmpty(maxEdit.text)
+                && (minEdit.text.toString().toInt() <= maxEdit.text.toString().toInt())
     }
 }
